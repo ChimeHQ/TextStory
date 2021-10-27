f636abae0abb0e5de2c976a86faf45b9d35d6211
Matt <85322+mattmassicotte@users.noreply.github.com>
2021-10-27 10:38:03 -0400

    New APIs to delay processing mutations

--- TextStory/LazyTextStoringMonitor.swift
+++ TextStory/LazyTextStoringMonitor.swift
@@ -4,27 +4,48 @@ public class LazyTextStoringMonitor {
     public let storingMonitor: TextStoringMonitor
     public private(set) var maximumProcessedLocation: Int
     public var minimumDelta: UInt = 1024
+    public var ignoreUnprocessedMutations = false
 
     public init(storingMonitor: TextStoringMonitor) {
         self.storingMonitor = storingMonitor
         self.maximumProcessedLocation = 0
     }
+
+    public func needsToProcessMutation(in range: NSRange) -> Bool {
+        guard ignoreUnprocessedMutations else {
+            return true
+        }
+
+        return range.location <= maximumProcessedLocation
+    }
 }
 
 extension LazyTextStoringMonitor: TextStoringMonitor {
     public func willApplyMutation(_ mutation: TextMutation, to storage: TextStoring) {
+        if needsToProcessMutation(in: mutation.range) == false {
+            return
+        }
+
         ensureTextProcessed(upTo: mutation.range.max, in: storage)
 
         storingMonitor.willApplyMutation(mutation, to: storage)
     }
 
     public func didApplyMutation(_ mutation: TextMutation, to storage: TextStoring, completionHandler: @escaping () -> Void) {
+        if needsToProcessMutation(in: mutation.range) == false {
+            return
+        }
+
         adjustMaximum(with: mutation, in: storage)
 
         storingMonitor.didApplyMutation(mutation, to: storage, completionHandler: completionHandler)
     }
 
     public func didCompleteChangeProcessing(of mutation: TextMutation?, in storage: TextStoring) {
+        if let mutation = mutation, needsToProcessMutation(in: mutation.range) == false {
+            return
+        }
+
         storingMonitor.didCompleteChangeProcessing(of: mutation, in: storage)
     }
 }
@@ -37,6 +58,14 @@ extension LazyTextStoringMonitor {
 
         precondition(maximumProcessedLocation >= 0)
         precondition(maximumProcessedLocation <= storage.length)
+    }
+
+    public func ensureTextProcessed(including location: Int, in storage: TextStoring) {
+        precondition(location <= storage.length)
+        
+        let actualLoc = min(location + 1, storage.length)
+
+        ensureTextProcessed(upTo: actualLoc, in: storage)
     }
 
     public func ensureTextProcessed(upTo location: Int, in storage: TextStoring) {
