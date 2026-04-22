1d4959d66aca1269b1f4ca03dcb366580b462d13
Matt <85322+mattmassicotte@users.noreply.github.com>
2026-04-22 06:54:17 -0400

    Move to 6.0

--- Package.swift
+++ Package.swift
@@ -1,36 +1,26 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
-    name: "TextStory",
+	name: "TextStory",
 	platforms: [
 		.macOS(.v10_15),
 		.macCatalyst(.v13),
 		.iOS(.v13),
 		.tvOS(.v13),
 	],
-    products: [
-        .library(name: "TextStory", targets: ["TextStory"]),
-        .library(name: "TextStoryTesting", targets: ["TextStoryTesting"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ChimeHQ/Rearrange", from: "2.0.0"),
-    ],
-    targets: [
-        .target(name: "Internal", publicHeadersPath: "."),
-        .target(name: "TextStory", dependencies: ["Internal", "Rearrange"]),
-        .target(name: "TextStoryTesting", dependencies: ["TextStory"]),
-        .testTarget(name: "TextStoryTests", dependencies: ["TextStory", "TextStoryTesting"]),
-    ]
+	products: [
+		.library(name: "TextStory", targets: ["TextStory"]),
+		.library(name: "TextStoryTesting", targets: ["TextStoryTesting"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/ChimeHQ/Rearrange", from: "2.0.0"),
+	],
+	targets: [
+		.target(name: "Internal", publicHeadersPath: "."),
+		.target(name: "TextStory", dependencies: ["Internal", "Rearrange"]),
+		.target(name: "TextStoryTesting", dependencies: ["TextStory"]),
+		.testTarget(name: "TextStoryTests", dependencies: ["TextStory", "TextStoryTesting"]),
+	]
 )
-
-let swiftSettings: [SwiftSetting] = [
-    .enableExperimentalFeature("StrictConcurrency")
-]
-
-for target in package.targets {
-    var settings = target.swiftSettings ?? []
-    settings.append(contentsOf: swiftSettings)
-    target.swiftSettings = settings
-}
